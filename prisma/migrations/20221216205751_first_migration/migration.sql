1a852f68de8be6beb8b7460a5ba9f728289d1982
stranavadavid <stranavadavid@protonmail.com>
2022-12-16 22:04:20 +0100

    Updated schema

--- prisma/migrations/20221216205751_first_migration/migration.sql
+++ prisma/migrations/20221216205751_first_migration/migration.sql
@@ -3,7 +3,6 @@ CREATE TABLE `User` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(191) NOT NULL,
     `email` VARCHAR(191) NOT NULL,
-    `emailVerified` DATETIME(3) NULL,
     `image` VARCHAR(191) NULL,
 
     UNIQUE INDEX `User_email_key`(`email`),
@@ -14,16 +13,12 @@ CREATE TABLE `User` (
 CREATE TABLE `Account` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `userId` INTEGER NOT NULL,
-    `type` VARCHAR(191) NOT NULL,
     `provider` VARCHAR(191) NOT NULL,
     `providerAccountId` VARCHAR(191) NOT NULL,
-    `refresh_token` TEXT NULL,
     `access_token` TEXT NULL,
     `expires_at` INTEGER NULL,
     `token_type` VARCHAR(191) NULL,
-    `scope` VARCHAR(191) NULL,
     `id_token` TEXT NULL,
-    `session_state` VARCHAR(191) NULL,
 
     INDEX `Account_userId_fkey`(`userId`),
     UNIQUE INDEX `Account_provider_providerAccountId_key`(`provider`, `providerAccountId`),
