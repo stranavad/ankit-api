1556d7aa43507a7bc24149b252af9a720b38f131
stranavadavid <stranavadavid@protonmail.com>
2023-01-13 19:53:46 +0100

    synced migrations

--- prisma/migrations/20230111200859_changed_default_position_to_0/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `Option` MODIFY `position` DOUBLE NOT NULL DEFAULT 10;
-
--- AlterTable
-ALTER TABLE `Question` MODIFY `position` DOUBLE NOT NULL DEFAULT 10;
--- prisma/migrations/20230112185547_changed_password_to_hash/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `password` on the `Questionnaire` table. The data in that column could be lost. The data in that column will be cast from `VarChar(191)` to `Char(60)`.
-
-*/
--- AlterTable
-ALTER TABLE `Questionnaire` MODIFY `password` CHAR(60) NULL;
